dbb3550b5934c80e5a346f1e8585593b29504360
Luftwaffles <sunnyhoang314@gmail.com>
2024-08-16 01:38:17 -0600

    Added typing text effect on about me

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const texts = ["undergraduate at the University of Calgary", "a programming enthusiast"];
+let currentTextIndex = 0;
+let charIndex = 0;
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const delayBetweenTexts = 2000; // Delay between texts
+
+const typingTextElement = document.getElementById("typing-text");
+
+function type() {
+    if (charIndex < texts[currentTextIndex].length) {
+        typingTextElement.textContent += texts[currentTextIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingSpeed);
+    } else {
+        setTimeout(erase, delayBetweenTexts);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typingTextElement.textContent = texts[currentTextIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingSpeed);
+    } else {
+        currentTextIndex = (currentTextIndex + 1) % texts.length;
+        setTimeout(type, typingSpeed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(type, delayBetweenTexts);
+});
