d6cea83e1a41c3c956c78fe170163fa6774a6494
Luftwaffles <sunnyhoang314@gmail.com>
2024-08-17 00:11:32 -0600

    update resume

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-const texts = ["undergraduate at the University of Calgary", "a programming enthusiast"];
+const texts = ["Undergraduate at the University of Calgary", "A programming enthusiast"];
 let currentTextIndex = 0;
 let charIndex = 0;
 const typingSpeed = 100;
